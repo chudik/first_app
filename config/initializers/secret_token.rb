103a2e385c6b9f4506ca3289ff7782708df61f71
Nastya Davidova <chudik@hotmail.com>
2011-03-09 17:29:29 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chudik::Application.config.secret_token = '9f516fdaa41c4ee4de6b3d60bd36b2351c09efcdc658ef1a440ff613d8b9519ad7158365c010ba1983ef7f926ff91ef7a426bc9dd014e3ead092dda56b6cc845'
